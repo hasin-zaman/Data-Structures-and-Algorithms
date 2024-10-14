4b81db30cdb4357d628eb4f85dc486677e0e9d12
Hasin <121337662+hasin-zaman@users.noreply.github.com>
2024-10-14 10:28:24 -0700

    populatingNextRightPointers (BFS)

---
+++ Binary Trees/BFS/populatingNextRightPointersInEachNode.js
@@ -0,0 +1,34 @@
+/**
+ * // Definition for a _Node.
+ * function _Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * @param {_Node} root
+ * @return {_Node}
+ */
+var connect = function(root) {
+    
+    let curr = root
+    let next = root ? root.left : null
+    while(curr && next) {
+        curr.left.next = curr.right
+
+        if(curr.next) {
+            curr.right.next = curr.next.left
+        }
+
+        curr = curr.next
+        if(!curr) {
+            curr = next
+            next = curr.left
+        }
+    }
+
+    return root
+};

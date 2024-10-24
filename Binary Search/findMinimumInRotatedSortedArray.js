89add5592ad83644c17f06bdc7ab2138d3b0a5db
Hasin <121337662+hasin-zaman@users.noreply.github.com>
2024-10-23 20:12:45 -0700

    findMinimumRotatedSortedArray (BS)

---
+++ Binary Search/findMinimumInRotatedSortedArray.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function(nums) {
+    let res = nums[0]
+    let l = 0
+    let r = nums.length - 1
+
+    while(l<=r) {
+        if(nums[l] < nums[r]) {
+            res = Math.min(res, nums[l])
+            break
+        }
+
+        let m = Math.floor((l+r)/2)
+        res = Math.min(res, nums[m])
+        if(nums[m] >= nums[l]) {
+            l = m + 1
+        }
+        else {
+            r = m - 1
+        }
+    }
+
+    return res
+};

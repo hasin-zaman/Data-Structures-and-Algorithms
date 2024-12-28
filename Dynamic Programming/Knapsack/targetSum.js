d26851901c85faaa710bacd1d7f3d814f699fde5
Hasin <121337662+hasin-zaman@users.noreply.github.com>
2024-12-28 05:09:31 +0400

    targetSum modification

--- Dynamic Programming/Knapsack/targetSum.js
+++ Dynamic Programming/Knapsack/targetSum.js
@@ -5,10 +5,10 @@
  */
 var findTargetSumWays = function(nums, target) {
     const sum = nums.reduce((a, b) => a + b, 0)
-    if((sum+target)%2 === 1 || target > sum) return 0
+    if ((sum + target) % 2 !== 0 || (sum + target) < 0) return 0;
     const halfSum = (sum + target)/2
 
-    const dp = Array(halfSum + 1).fill(0)
+    const dp = new Array(halfSum + 1).fill(0)
     dp[0] = 1
 
     for(const num of nums) {
